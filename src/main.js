f73930d09db48e95c1db6fa5d74c65483d2c4f90
NCSpikeZ <nicolas.coopman@student.heaj.be>
2023-11-13 16:44:58 +0100

    erreur

--- src/main.js
+++ src/main.js
@@ -3,3 +3,12 @@ import App from './App.vue'
 import './assets/css/tailwind.css'
 
 createApp(App).mount('#app')
+
+import router from './router';
+
+createApp.config.productionTip = false;
+
+new createApp({
+  render: (h) => h(App),
+  router,
+}).$mount('#app');
